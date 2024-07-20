f5881cf22c09d45de3b1b93f9dcc6e8591a09d1e
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-20 00:39:13 -0600

    Refactor main.go and to service package

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,11 +4,15 @@ import (
 	"github.com/eduardolat/pgbackweb/internal/config"
 	"github.com/eduardolat/pgbackweb/internal/database"
 	"github.com/eduardolat/pgbackweb/internal/database/dbgen"
+	"github.com/eduardolat/pgbackweb/internal/service"
 )
 
 func main() {
 	env := config.GetEnv()
+
 	db := database.Connect(env)
 	defer db.Close()
-	_ = dbgen.New(db)
+	dbgen := dbgen.New(db)
+
+	_ = service.New(dbgen)
 }
