5b561ff86998b99bbbd2aeb257c57ca6578ffcd7
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-20 00:56:07 -0600

    Add sessions table migration

---
+++ internal/database/migrations/20240720055720_add_sessions_table.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS sessions (
+  id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  user_id UUID NOT NULL REFERENCES users(id),
+
+  token TEXT NOT NULL,
+
+  ip TEXT NOT NULL,
+  user_agent TEXT NOT NULL,
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+);
+
+CREATE INDEX IF NOT EXISTS
+idx_sessions_user_id ON sessions(user_id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS sessions;
+-- +goose StatementEnd
