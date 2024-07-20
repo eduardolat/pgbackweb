b16142a509c05205675590f047194f6b83e5c87d
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-20 17:33:03 -0600

    Update CreateUser function to hash user password using bcrypt

--- internal/service/users/create_user.go
+++ internal/service/users/create_user.go
@@ -4,10 +4,17 @@ import (
 	"context"
 
 	"github.com/eduardolat/pgbackweb/internal/database/dbgen"
+	"github.com/eduardolat/pgbackweb/internal/util/cryptoutil"
 )
 
 func (s *Service) CreateUser(
 	ctx context.Context, params dbgen.UsersServiceCreateUserParams,
 ) (dbgen.User, error) {
+	hash, err := cryptoutil.CreateBcryptHash(params.Password)
+	if err != nil {
+		return dbgen.User{}, err
+	}
+	params.Password = hash
+
 	return s.dbgen.UsersServiceCreateUser(ctx, params)
 }
