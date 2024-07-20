bb1ccaf4eaf50b1765e53d349dd937d39010289d
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-19 18:31:32 -0600

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./internal/view/web/**/*.go'],
+  plugins: [require('daisyui')],
+  daisyui: {
+    logs: false,
+    themes: ["light", "dim"],
+    darkTheme: "dim",
+  },
+  theme: {
+    screens: {
+      'desk': '768px', // only one breakpoint to keep it simple
+    },
+    extend: {},
+  }
+}
