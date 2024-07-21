88df185ab7d25b9d39d052825ba7909834535068
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-21 17:28:11 -0600

    Add backup_id column to executions table and create index

--- internal/database/migrations/20240720060508_add_executions_table.sql
+++ internal/database/migrations/20240720060508_add_executions_table.sql
@@ -2,6 +2,7 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS executions (
   id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  backup_id UUID NOT NULL REFERENCES backups(id),
 
   status TEXT NOT NULL CHECK (
     status IN ('running', 'success', 'failed', 'deleted')
@@ -17,6 +18,9 @@ CREATE TABLE IF NOT EXISTS executions (
 
 CREATE TRIGGER executions_change_updated_at
 BEFORE UPDATE ON executions FOR EACH ROW EXECUTE FUNCTION change_updated_at();
+
+CREATE INDEX IF NOT EXISTS
+idx_executions_backup_id ON executions(backup_id);
 -- +goose StatementEnd
 
 -- +goose Down
