e693d1e11ffb00d8a4b025cd1b8c846ef6e1074c
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-20 22:22:58 -0600

    Update database connection encryption and version field

--- internal/service/databases/update_database.sql
+++ internal/service/databases/update_database.sql
@@ -2,7 +2,7 @@
 UPDATE databases
 SET
   name = @name,
-  connection_string = pgp_sym_encrypt(@connection_string, @encryption_key)
+  connection_string = pgp_sym_encrypt(@connection_string, @encryption_key),
   pg_version = @pg_version
 WHERE id = @id
 RETURNING *;
