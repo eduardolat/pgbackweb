45c1fec5a7a1b0743c071ae4102aad2741d72457
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-22 23:04:18 -0600

    Add middleware for injecting request context

---
+++ internal/view/middleware/inject_reqctx.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/eduardolat/pgbackweb/internal/database/dbgen"
+	"github.com/eduardolat/pgbackweb/internal/logger"
+	"github.com/eduardolat/pgbackweb/internal/view/reqctx"
+	"github.com/labstack/echo/v4"
+)
+
+func (m *Middleware) InjectReqctx(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		found, user, err := m.servs.AuthService.GetUserFromSessionCookie(c)
+		if err != nil {
+			logger.Error("failed to get user from session cookie", logger.KV{
+				"ip":    c.RealIP(),
+				"ua":    c.Request().UserAgent(),
+				"error": err,
+			})
+			return c.String(http.StatusInternalServerError, "Internal server error")
+		}
+
+		if !found {
+			return next(c)
+		}
+
+		reqctx.SetCtx(c, reqctx.Ctx{
+			IsAuthed: true,
+			User: dbgen.User{
+				ID:        user.ID,
+				Name:      user.Name,
+				Email:     user.Email,
+				CreatedAt: user.CreatedAt,
+				UpdatedAt: user.UpdatedAt,
+			},
+		})
+		return next(c)
+	}
+}
