cf2f8c4d96cd2b016914f46a10bf31779828e7fb
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-22 21:34:48 -0600

    Update web router to use rate limiting middleware

--- internal/view/web/auth/router.go
+++ internal/view/web/auth/router.go
@@ -1,7 +1,10 @@
 package auth
 
 import (
+	"time"
+
 	"github.com/eduardolat/pgbackweb/internal/service"
+	"github.com/eduardolat/pgbackweb/internal/view/middleware"
 	"github.com/labstack/echo/v4"
 )
 
@@ -9,12 +12,17 @@ type handlers struct {
 	servs *service.Service
 }
 
-func MountRouter(parent *echo.Group, servs *service.Service) {
+func MountRouter(
+	parent *echo.Group, mids *middleware.Middleware, servs *service.Service,
+) {
 	h := handlers{servs: servs}
 
 	parent.GET("/create-first-user", h.createFirstUserPageHandler)
 	parent.POST("/create-first-user", h.createFirstUserHandler)
 
 	parent.GET("/login", h.loginPageHandler)
-	parent.POST("/login", h.loginHandler)
+	parent.POST("/login", h.loginHandler, mids.RateLimit(middleware.RateLimitConfig{
+		Limit:  5,
+		Period: 10 * time.Second,
+	}))
 }
--- internal/view/web/router.go
+++ internal/view/web/router.go
@@ -2,10 +2,13 @@ package web
 
 import (
 	"github.com/eduardolat/pgbackweb/internal/service"
+	"github.com/eduardolat/pgbackweb/internal/view/middleware"
 	"github.com/eduardolat/pgbackweb/internal/view/web/auth"
 	"github.com/labstack/echo/v4"
 )
 
-func MountRouter(parent *echo.Group, servs *service.Service) {
-	auth.MountRouter(parent.Group("/auth"), servs)
+func MountRouter(
+	parent *echo.Group, mids *middleware.Middleware, servs *service.Service,
+) {
+	auth.MountRouter(parent.Group("/auth"), mids, servs)
 }
