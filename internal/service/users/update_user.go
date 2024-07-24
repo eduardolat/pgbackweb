836c671da08fd5046ca8a2ff20e47a2f6fa8b51b
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-23 18:56:43 -0600

    Hash user password in UpdateUser function

--- internal/service/users/update_user.go
+++ internal/service/users/update_user.go
@@ -4,10 +4,19 @@ import (
 	"context"
 
 	"github.com/eduardolat/pgbackweb/internal/database/dbgen"
+	"github.com/eduardolat/pgbackweb/internal/util/cryptoutil"
 )
 
 func (s *Service) UpdateUser(
 	ctx context.Context, params dbgen.UsersServiceUpdateUserParams,
 ) (dbgen.User, error) {
+	if params.Password.Valid {
+		hashedPassword, err := cryptoutil.CreateBcryptHash(params.Password.String)
+		if err != nil {
+			return dbgen.User{}, err
+		}
+		params.Password.String = hashedPassword
+	}
+
 	return s.dbgen.UsersServiceUpdateUser(ctx, params)
 }
