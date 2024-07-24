c2fe439c88d93e5d26ac6944812c2783fd77296b
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-07-24 09:26:41 -0600

    Update about page content and layout

--- internal/view/web/dashboard/about/index.go
+++ internal/view/web/dashboard/about/index.go
@@ -19,52 +19,61 @@ func indexPage() gomponents.Node {
 	content := []gomponents.Node{
 		component.H1Text("About PG Back Web"),
 		html.Div(
-			html.Class("max-w-[600px] mt-1"),
-			component.PText(`
-				PG Back Web was born in July 2024 out of a need for a simple and
-				user-friendly backup solution for self-hosted PostgreSQL databases.
-				After searching extensively for an easy-to-use backup tool and not
-				finding one, I decided to create my own. Its mission is to provide a
-				straightforward web interface that makes managing PostgreSQL backups
-				effortless and efficient.
-			`),
+			html.Class("grid grid-cols-2 gap-4 mt-4"),
 
-			html.Table(
-				html.Class("table border mt-4"),
-				html.Tr(
-					html.Td(component.SpanText("License")),
-					html.Td(
-						html.A(
-							html.Class("link"),
-							html.Href("https://github.com/eduardolat/pgbackweb/blob/main/LICENSE"),
-							html.Target("_blank"),
-							component.SpanText("MIT"),
+			component.CardBox(component.CardBoxParams{
+				Children: []gomponents.Node{
+					component.PText(`
+						PG Back Web was born in July 2024 out of a need for a simple and
+						user-friendly backup solution for self-hosted PostgreSQL databases.
+						After searching extensively for an easy-to-use backup tool and not
+						finding one, I decided to create my own. Its mission is to provide a
+						straightforward web interface that makes managing PostgreSQL backups
+						effortless and efficient.
+					`),
+				},
+			}),
+
+			component.CardBox(component.CardBoxParams{
+				Children: []gomponents.Node{
+					html.Table(
+						html.Class("table"),
+						html.Tr(
+							html.Th(component.SpanText("License")),
+							html.Td(
+								html.A(
+									html.Class("link"),
+									html.Href("https://github.com/eduardolat/pgbackweb/blob/main/LICENSE"),
+									html.Target("_blank"),
+									component.SpanText("MIT"),
+								),
+							),
+						),
+						html.Tr(
+							html.Th(component.SpanText("About the author")),
+							html.Td(
+								html.A(
+									html.Class("link"),
+									html.Href("https://eduardo.lat"),
+									html.Target("_blank"),
+									component.SpanText("https://eduardo.lat"),
+								),
+							),
+						),
+						html.Tr(
+							html.Th(component.SpanText("Repository")),
+							html.Td(
+								html.A(
+									html.Class("link"),
+									html.Href("https://github.com/eduardolat/pgbackweb"),
+									html.Target("_blank"),
+									component.SpanText("https://github.com/eduardolat/pgbackweb"),
+								),
+							),
 						),
 					),
-				),
-				html.Tr(
-					html.Td(component.SpanText("About the author")),
-					html.Td(
-						html.A(
-							html.Class("link"),
-							html.Href("https://eduardo.lat"),
-							html.Target("_blank"),
-							component.SpanText("https://eduardo.lat"),
-						),
-					),
-				),
-				html.Tr(
-					html.Td(component.SpanText("Repository")),
-					html.Td(
-						html.A(
-							html.Class("link"),
-							html.Href("https://github.com/eduardolat/pgbackweb"),
-							html.Target("_blank"),
-							component.SpanText("https://github.com/eduardolat/pgbackweb"),
-						),
-					),
-				),
-			),
+				},
+			}),
 		),
 	}
 
