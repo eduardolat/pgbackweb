a4c04c626329acdc3c9147ef06b6cb4b642467e1
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-08-04 18:59:16 -0600

    Add backup_is_local column to GetExecution SQL query

--- internal/service/executions/get_execution.sql
+++ internal/service/executions/get_execution.sql
@@ -1,7 +1,8 @@
 -- name: ExecutionsServiceGetExecution :one
 SELECT
   executions.*,
-  databases.pg_version AS database_pg_version
+  databases.pg_version AS database_pg_version,
+  backups.is_local AS backup_is_local
 FROM executions
 INNER JOIN backups ON backups.id = executions.backup_id
 INNER JOIN databases ON databases.id = backups.database_id
