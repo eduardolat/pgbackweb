3837c958e4f3faae7584e1046ea95617f6e0931c
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-09-06 00:33:20 -0600

    Refactor HTMX initialization to ensure observer setup occurs after DOM content is loaded

--- internal/view/static/js/init-htmx.js
+++ internal/view/static/js/init-htmx.js
@@ -57,14 +57,16 @@ export function initHTMX () {
 
   // This fixes this issue:
   // https://stackoverflow.com/questions/73658449/htmx-request-not-firing-when-hx-attributes-are-added-dynamically-from-javascrip
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      mutation.addedNodes.forEach((node) => {
-        if (node.nodeType === 1 && !node['htmx-internal-data']) {
-          htmx.process(node)
-        }
+  document.addEventListener('DOMContentLoaded', function () {
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach((node) => {
+          if (node.nodeType === 1 && !node['htmx-internal-data']) {
+            htmx.process(node)
+          }
+        })
       })
     })
+    observer.observe(document, { childList: true, subtree: true })
   })
-  observer.observe(document, { childList: true, subtree: true })
 }
