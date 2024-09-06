3c7f417979bbbef55f6a533c356dd5917c47b962
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-09-06 00:30:03 -0600

    Add active state check and update on history changes in dashboard aside item component

--- internal/view/static/js/alpine-components/dashboard-aside-item.js
+++ internal/view/static/js/alpine-components/dashboard-aside-item.js
@@ -4,13 +4,30 @@ export const dashboardAsideItem = {
     link,
     strict,
     is_active: false,
-    init () {
+
+    checkActive () {
       if (this.strict) {
         this.is_active = window.location.pathname === this.link
         return
       }
 
       this.is_active = window.location.pathname.startsWith(this.link)
+    },
+
+    init () {
+      this.checkActive()
+
+      const originalPushState = window.history.pushState
+      window.history.pushState = (...args) => {
+        originalPushState.apply(window.history, args)
+        this.checkActive()
+      }
+
+      const originalReplaceState = window.history.replaceState
+      window.history.replaceState = (...args) => {
+        originalReplaceState.apply(window.history, args)
+        this.checkActive()
+      }
     }
   })
 }
