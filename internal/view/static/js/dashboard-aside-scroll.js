4cfab676465042f1f518b1e968d938be808e69af
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-09-06 19:53:31 -0600

    Refactor dashboard aside scroll initialization for better structure and debouncing logic

--- internal/view/static/js/dashboard-aside-scroll.js
+++ internal/view/static/js/dashboard-aside-scroll.js
@@ -1,20 +1,19 @@
 export function initDashboardAsideScroll () {
-  const el = document.getElementById('dashboard-aside')
-  const key = 'dashboard-aside-scroll-position'
-
-  if (!el) return
-
-  const saveScrollPosition = window.debounce(
-    () => {
-      const scrollPosition = el.scrollTop
-      localStorage.setItem(key, scrollPosition)
-      console.log(scrollPosition)
-    },
-    200
-  )
-  el.addEventListener('scroll', saveScrollPosition)
-
   document.addEventListener('DOMContentLoaded', function () {
+    const el = document.getElementById('dashboard-aside')
+    const key = 'dashboard-aside-scroll-position'
+
+    if (!el) return
+
+    const saveScrollPosition = window.debounce(
+      () => {
+        const scrollPosition = el.scrollTop
+        localStorage.setItem(key, scrollPosition)
+      },
+      200
+    )
+    el.addEventListener('scroll', saveScrollPosition)
+
     const scrollPosition = localStorage.getItem(key)
     if (scrollPosition) {
       el.scrollTop = parseInt(scrollPosition, 10)
